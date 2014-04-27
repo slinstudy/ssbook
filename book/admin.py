bb3ef10983b284370bf3ee7406a63ac18f45543f
sunhome <slinstudy@gmail.com>
2014-04-27 15:55:57 +0800

    home2

--- book/admin.py
+++ book/admin.py
@@ -5,7 +5,10 @@ from .models import *
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id','isbn', 'title', 'author', 'publisher', 'pubdate')
-
+    # list_display = ('id','isbn', 'title', 'author', 'publisher', 'pubdate')
+	list_display = ('id','isbn', 'title', 'author','translator','publisher',)
+	list_filter = ('publisher',)
+	search_fields = ('id','title','isbn',)
+	list_per_page=20
 
 admin.site.register(Book,BookAdmin)
